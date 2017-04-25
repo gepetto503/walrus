148a7f6c33466b4c05cc097ac39d7e3842af9b5d
Brendan Hanna and Dylan Dills <brendan@>
2017-04-25 11:57:55 -0700

    add fadetoggle and slidetoggle to morewalrus.html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
+// $(document).ready(function() {
+//   $(".clickable").click(function() {
+//       $("#initially-hidden").toggle();
+//       $("initially-hidden").fadeIn('slow');
+//       $("#initially-showing").toggle();
+//   });
+// });
 $(document).ready(function() {
+
   $(".clickable").click(function() {
-      $("#initially-hidden").toggle();
-      $("#initially-showing").toggle();
-    });
+    $("#initially-hidden").fadeToggle();
+  });
+
+  $(".clickable").click(function() {
+      $('#initially-showing').fadeToggle();
+
+  });
+
+  $(".walruses").click(function() {
+      $('.walruses').slideToggle();
+  });
+
+  $(".clickable").click(function() {
+    $('.walruses').slideToggle();
+
+  });
+
+  $(".walruses").click(function() {
+    $('#comeback').fadeToggle();
+
+  });
+
 });
